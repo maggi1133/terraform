2d85e59370358c33dbdbead14c2be7850cdc9a9e
sivakumarreddy <info@joindevops.com>
2023-09-08 08:33:04 +0530

    terraform

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.15.0"
     }
   }
+
+  backend "s3" {
+    bucket   = "roboshop-remote-state"
+    key = "foreach-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+  }
 }
 
 provider "aws" {
